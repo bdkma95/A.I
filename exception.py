f0217bbb420d075a75a3916cab85630c1247fc43
Boris <68553253+bdkma95@users.noreply.github.com>
2025-02-21 19:08:29 +0100

    Create exception.py

---
+++ exception.py
@@ -0,0 +1,8 @@
+class SafetyAPIError(Exception):
+    """Custom exception for safety API failures"""
+
+class TranslationError(Exception):
+    """Custom exception for translation failures"""
+
+class RecommendationError(Exception):
+    """Custom exception for recommendation failures"""
